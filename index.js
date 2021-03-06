9294414a921c9c65f6d44d051ab575a2bfc34fd8
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-03-06 21:17:35 +0530

    Uploadig

---
+++ index.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("1");
+const title = document.getElementById("title");
+
+
+button.addEventListener("click", function(){ 
+  
+  fetch('http://api.open-notify.org/iss-now.json')
+    .then(resp=>resp.json())
+    .then(result=>
+        title.textContent=(`International Space Station Current Location is Longitude: ${result.iss_position.longitude} and the Latitude is ${result.iss_position.latitude}`));
+    
+
+}
+    );
+
+
+
