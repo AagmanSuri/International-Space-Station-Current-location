125d59caed641c13b118397283c85b4abe99e2ab
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-04-23 23:49:03 +0530

    adding alert if not working

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const button = document.getElementById("one");
 const title = document.getElementById("title");
 
+function errorHandel(error){
+alert("API Problem . Please try after some time !!!!!");
+console.log("Server(API) Problem Please try after some time !!!!!");
+}
 
 button.addEventListener("click", function(){ 
   
   fetch('http://api.open-notify.org/iss-now.json')
     .then(resp=>resp.json())
     .then(result=>
-        title.textContent=(`International Space Station Current Location is Longitude: ${result.iss_position.longitude} and the Latitude : ${result.iss_position.latitude}`));
-    
+        title.textContent=(`International Space Station Current Location is Longitude: 
+        ${result.iss_position.longitude} and 
+        the Latitude : ${result.iss_position.latitude}`))
+        .catch(errorHandel)
 
 }
+
     );
 
 
